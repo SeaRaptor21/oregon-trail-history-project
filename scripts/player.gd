617c1cf74c1f7aeebd8613c2ef27e5f7795fb13f
Awafoo <159366570+Awafoo@users.noreply.github.com>
2024-02-13 17:27:45 -0800

    Made some links in credits

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,7 @@ var dist_from_path = 0
 func update_pos(collision):
 	position = layers[layer].to_global(layers[layer].map_to_local(pos))
 	position.y -= (281-163)/8.0
-	print(int(str(collision.get_collider().name)[-1]))
+		print(int(str(collision.get_collider().name)[-1]))
 	#if collision != null:
 	#	print(collision.collider.name)
 
